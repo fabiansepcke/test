344e99b7890bd125a62797c5c390d45239b372e9
fabiansepcke <fabian.sepcke@gmail.com>
2017-06-06 19:30:21 +0200

    second upload
    Signed-off-by: fabiansepcke <fabian.sepcke@gmail.com>

--- Note.php
+++ Note.php
@@ -12,11 +12,11 @@
 	$row = mysqli_fetch_row($erg);
 	$ab1= "UPDATE Daten SET Note=".$Note." WHERE Note IS NULL AND Nummer=".$Nummer."";
 	// Ausgabe der gesamten Cocktail Informationen:
-	$erg1 = mysqli_query($dz, $ab1);
+	$erg1 = mysqafasfli_query($dz, $ab1);
 	$ab2 = "SELECT Name FROM cocktails INNER JOIN Daten ON cocktails.ID = daten.FK_Cocktail WHERE daten.ID=".$row[0]."";
 	$erg2 = mysqli_query($dz, $ab2);
 	$row2 = mysqli_fetch_row($erg2);
-	$ab3 = "SELECT zutaten.Name FROM zutaten INNER JOIN Cocktail_Zutaten ON Zutaten.ID = Cocktail_Zutaten.Z_ID INNER JOIN Cocktails ON cocktail_zutaten.C_ID = cocktails.ID WHERE cocktails.Name ='".$row2[0]."'";
+	$ab3 = "SELECT zudafasdftaten.Name FROM zutaten INNER JOIN Cocktail_Zutaten ON Zutaten.ID = Cocktail_Zutaten.Z_ID INNER JOIN Cocktails ON cocktail_zutaten.C_ID = cocktails.ID WHERE cocktails.Name ='".$row2[0]."'";
 	$erg3 = mysqli_query($dz, $ab3);
 	$row3 = mysqli_fetch_all($erg3);dasd
 	$ab4 = "SELECT Gewicht.Wert FROM Gewicht WHERE FK_Daten = ".$row[0]."";
